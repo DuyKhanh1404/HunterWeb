89af16c026640175b05ad47af10e7b82a0f86c02
DuyKhanh1404 <dkn0609@gmail.com>
2026-09-20 18:30:35 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'hunter-system-offline-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './favicon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
