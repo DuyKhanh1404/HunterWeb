058833161fc28dd53868b74e0d1bf73c7a5ee505
DuyKhanh1404 <dkn0609@gmail.com>
2026-09-20 18:36:05 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,29 @@
 const CACHE_NAME = 'hunter-system-offline-v1';
 const ASSETS = [
   './',
-  './index.html',
-  './favicon.png'
+  './index.html'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))).then(() => self.clients.claim()));
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request).catch(() => caches.match('./index.html')))
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request).catch(() => caches.match('./index.html'));
+    })
   );
 });
