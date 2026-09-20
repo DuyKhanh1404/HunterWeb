be54fdbd9006122137d3238e9c7aca348f4be9ed
DuyKhanh1404 <dkn0609@gmail.com>
2026-09-20 20:48:07 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,25 @@
 const CACHE_NAME = 'hunter-system-offline-v1';
 const ASSETS = [
   './',
-  './index.html'
+  './index.html',
+  './favicon.png'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))).then(() => self.clients.claim()));
 });
 
 self.addEventListener('fetch', (e) => {
+  // Chỉ can thiệp cache đối với các request GET (tránh lỗi 405 khi sync/reset dữ liệu lên Cloud)
+  if (e.request.method !== 'GET') {
+    return;
+  }
+
   e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request).catch(() => caches.match('./index.html'));
-    })
+    caches.match(e.request).then(res => res || fetch(e.request).catch(() => caches.match('./index.html')))
   );
 });
